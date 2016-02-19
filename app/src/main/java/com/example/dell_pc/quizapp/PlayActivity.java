be6c6eb27f1194c5fdc5ae1bbb750f1546fd987e
Ben Webster <bmwebster@wisc.edu>
2016-02-19 15:53:53 -0600

    app completed

--- app/src/main/java/com/example/dell_pc/quizapp/PlayActivity.java
+++ app/src/main/java/com/example/dell_pc/quizapp/PlayActivity.java
@@ -1,30 +1,55 @@
 package com.example.dell_pc.quizapp;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 /**
- * Created by DELL-PC on 2/18/2016.
+ * Created by DELL-PC on 2/19/2016.
  */
-public class PlayActivity  extends AppCompatActivity {
+public class PlayActivity extends AppCompatActivity{
+    public static int numCorrect = 0;
+  //  public static PlayActivity activity;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
-      /*  Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
+    //    activity = this;
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_fragment_container, new QuestionOne())
+                .addToBackStack(null)
+                .commit();
     }
 
+  //  @Override
+  /*  public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_play, menu);
+        getMenuInflater()
+
+        return true;
+    }
+*/
+   /* public void  reset() {
+        numCorrect = 0;
+        Intent mainActivity = new Intent(this, MainActivity.class);
+        startActivity(mainActivity);
+    }*/
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
---
+++ app/src/main/java/com/example/dell_pc/quizapp/QuestionOne.java
@@ -0,0 +1,106 @@
+package com.example.dell_pc.quizapp;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.content.Context;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link QuestionOne#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class QuestionOne extends Fragment {
+    String correctAnswer = "Nitrogen";
+    String userAnswer;
+    EditText answer1Text;
+    private Button submit1;
+    Context context;
+   // int numCorrect = 0;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    public QuestionOne() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment QuestionOne.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static QuestionOne newInstance(String param1, String param2) {
+        QuestionOne fragment = new QuestionOne();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = null;
+        view = inflater.inflate(R.layout.question1, container, false);
+
+        submit1 = (Button)view.findViewById(R.id.submitButton1);
+        return view;
+    }
+
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //TA Implementation
+
+        //different way of implementing click interaction.
+        submit1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String answer;
+                answer1Text = (EditText)getActivity().findViewById(R.id.answer1);
+                userAnswer = answer1Text.getText().toString();
+                answer = userAnswer.trim();
+                if (answer.equalsIgnoreCase(correctAnswer)) {
+                    PlayActivity.numCorrect++;
+                }
+                getFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.main_fragment_container, new QuestionTwo())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
+
+}
